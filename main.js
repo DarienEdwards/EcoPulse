0e47dcc35d4fc7587dda6ee454499b4eeefad14f
Darien Edwards <122249097+DarienEdwards@users.noreply.github.com>
2025-06-09 15:37:11 -0400

    Add scroll-to-top button

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const scrollBtn = document.getElementById('scrollTopBtn');
 
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 200) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
