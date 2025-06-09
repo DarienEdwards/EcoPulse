2060871db2b3d0f7977e6030779f0c0e49915888
Darien Edwards <122249097+DarienEdwards@users.noreply.github.com>
2025-06-09 15:37:04 -0400

    Add scroll-to-top floating button

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const scrollBtn = document.getElementById('scrollTopBtn');
+
+function toggleScrollButton() {
+  if (window.pageYOffset > 300) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+window.addEventListener('scroll', toggleScrollButton);
+scrollBtn.addEventListener('click', scrollToTop);
 
